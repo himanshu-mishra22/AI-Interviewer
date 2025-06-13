d413863eb9bbe9b7e69f9a0dc75833ad2984b3d1
HIMANSHU PRAKASH MISHRA <mhimanshuprakash@gmail.com>
2025-06-13 22:23:43 +0530

    dependencies installed in frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
-
+import { BrowserRouter, Routes, Route} from 'react-router'
+import {Toaster} from 'react-hot-toast'
+import Login from './pages/auth/Login'
+import Signup from './pages/auth/Signup'
+import Dashboard from './pages/home/Dashboard'
+import LandingPage from './pages/LandingPage'
+import InterviewPrep from './pages/InterviewPrep/InterviewPrep'
 const App = () => {
   return (
-    <div className='text-5xl bg-amber-700'>App</div>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<LandingPage/>} />
+          <Route path='/login' element={<Login/>} />
+          <Route path='/signup' element={<Signup/>} />
+          <Route path='/dashboard' element={<Dashboard/>} />
+          <Route path='/interview/:sessionId' element={<InterviewPrep/>} />
+        </Routes>
+      </BrowserRouter>
+
+      <Toaster 
+        toastOptions={{
+          className:"",
+          style:{
+            fontSize:"13px",
+          }
+        }}
+        />
+    </div>
   )
 }
 
