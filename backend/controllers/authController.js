53fda3c8c77fd70077f28296d5fdfc0ccbbe5b8c
HIMANSHU PRAKASH MISHRA <mhimanshuprakash@gmail.com>
2025-06-22 22:27:58 +0530

    login and profile api created

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -40,11 +40,42 @@ const registerUser = async (req,res)=>{
 }
 
 const loginUser = async(req,res)=>{
+    try{
+        const {email,password} = req.body;
+        const user = await User.findOne({email});
+        if(!user){
+            return res.status(500).json({message:"Invalid username or password"});
 
+        }
+
+        const isMatch = await bcrypt.compare(password,user.password);
+        if(!isMatch){
+            return res.status(500).json({message:"Invalid username or password"});
+        }
+
+        res.json({
+            _id:user._id,
+            name:user.name,
+            email:user.email,
+            profilePicUrl:user.profilePicUrl,
+            token:generateToken(user._id)
+        })
+
+    }catch(err){
+        res.status(500).json({message:"Server error",err})
+    }
 }
 
 const getUserProfile = async(req,res)=>{
-
+    try {
+        const user = await User.findById(req.user._id).select("-password");
+        if(!user){
+            return res.status(404).json({message:"User not found"});
+        }
+        res.json(user);
+    } catch (error) {
+        return res.status(500).json({message:"Server error",error})
+    }
 }
 
 module.exports = {registerUser,loginUser,getUserProfile}
