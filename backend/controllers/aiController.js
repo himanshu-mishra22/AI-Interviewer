294140f726eb46a6c20a24d768e575e234a4ac8d
HIMANSHU PRAKASH MISHRA <mhimanshuprakash@gmail.com>
2025-07-04 00:30:12 +0530

    Interview questions generations completed

--- backend/controllers/aiController.js
+++ backend/controllers/aiController.js
@@ -1,11 +1,28 @@
 const {GoogleGenAI} = require("@google/genai");
-const {conceptExplainPrompt} = require("../utils/prompts");
+const {conceptExplainPrompt, questionAnswerPrompt} = require("../utils/prompts");
 
 const ai = new GoogleGenAI({apiKey:process.env.GEMINI_API_KEY});
 
 const generateInterviewQuestions = async(req,res)=>{
     try{
+        const {role,experience,topicsToFocus,numberOfQuestions} = req.body;
+        if(!role || !experience || !topicsToFocus || !numberOfQuestions){
+            return res.status(400).json({message:"Missing required fields"});
+        }
 
+        const prompt = questionAnswerPrompt(role,experience,topicsToFocus,numberOfQuestions);
+        const response = await ai.models.generateContent({
+            model:"gemeni-2.0-flash-lite",
+            contents:prompt,
+        });
+        let rawText = response.text;
+        // console.log(rawText);
+
+        const realtext = rawText.replace(/^```json\s*/,"").replace(/```$/,"").trim();
+        console.log(realtext);
+
+        const data = JSON.parse(realtext);
+        res.send(200).json(data);
     }catch(e){
         res.status(500).json({message:"Failed to generate questions", error:e.message});
     }
