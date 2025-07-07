8dcc1d67630ccffd71341723b9eabcc04a021baa
HIMANSHU PRAKASH MISHRA <mhimanshuprakash@gmail.com>
2025-07-07 22:19:32 +0530

    explanation generation api completed

--- backend/controllers/aiController.js
+++ backend/controllers/aiController.js
@@ -1,4 +1,5 @@
 const {GoogleGenAI} = require("@google/genai");
+require('dotenv').config();
 const {conceptExplainPrompt, questionAnswerPrompt} = require("../utils/prompts");
 
 const ai = new GoogleGenAI({apiKey:process.env.GEMINI_API_KEY});
@@ -12,17 +13,17 @@ const generateInterviewQuestions = async(req,res)=>{
 
         const prompt = questionAnswerPrompt(role,experience,topicsToFocus,numberOfQuestions);
         const response = await ai.models.generateContent({
-            model:"gemeni-2.0-flash-lite",
+            model:"gemini-2.0-flash",
             contents:prompt,
         });
         let rawText = response.text;
-        // console.log(rawText);
+        // console.log("raw text:",  rawText);
 
         const realtext = rawText.replace(/^```json\s*/,"").replace(/```$/,"").trim();
-        console.log(realtext);
+        // console.log("data: ",realtext);
 
         const data = JSON.parse(realtext);
-        res.send(200).json(data);
+        res.status(200).json(data);
     }catch(e){
         res.status(500).json({message:"Failed to generate questions", error:e.message});
     }
@@ -31,7 +32,23 @@ const generateInterviewQuestions = async(req,res)=>{
 
 const generateExplanation= async(req,res)=>{
     try{
+        const {question} = req.body;
+        if(!question){
+            return res.status(400).json({message:"Missing required fields"});
+        }
 
+        const prompt = conceptExplainPrompt(question);
+        const response = await ai.models.generateContent({
+             model:"gemini-2.0-flash",
+            contents:prompt
+        });
+
+        let rawText = response.text;
+        const realtext = rawText.replace(/^```json\s*/,"").replace(/```$/,"").trim();
+        const data = JSON.parse(realtext);
+        res.status(200).json(data);
+
+        
     }catch(e){
         res.status(500).json({message:"Failed to generate explanation", error:e.message});
     }
