b79628a8b21283689816606034c00b8d04326067
HIMANSHU PRAKASH MISHRA <mhimanshuprakash@gmail.com>
2025-08-01 13:10:06 +0530

    ports updated

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(
     cors({
-        origin:'*',
+        origin:process.env.FRONTEND,
         methods:["GET","POST", "PUT", "DELETE"],
         allowedHeaders:["Content-Type", "Authorization"],
     })
